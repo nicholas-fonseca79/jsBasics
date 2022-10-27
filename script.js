ae8a492f785224efe97fef273f70f3b353b71cf9
nick fonseca <nick.fonseca79@gmail.com>
2022-10-27 16:04:32 -0700

    Variables, Strings, Numbers, Booleans

---
+++ script.js
@@ -0,0 +1,30 @@
+/* let variableName = 'variables and dataTypes';
+
+const $name = 'Nick';
+
+variableName = 'attempting to reassign';
+
+console.log(variableName);
+console.log($name); */
+
+// ** STRINGS are mostly use for text **
+
+//const stringExample = 'What is up world?';
+
+// ** Numbers
+/* const wholeNumber = 22;
+const  decimalNumber = 0.55;
+
+console.log(wholeNumber);
+console.log(decimalNumber);
+
+const adding = wholeNumber + decimalNumber; */
+
+// ** Booleans true or false
+const coolGuy = true;
+
+if(coolGuy) {
+    console.log('I am cool');
+} else {
+    console.log('I am NOT cool');
+}
