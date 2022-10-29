e86152dd10747d2c5ed20eaeb543fba555c263c9
nick fonseca <nick.fonseca79@gmail.com>
2022-10-28 22:36:29 -0700

    Variables and dataTypes lesson complete

--- variablesAndDataTypes.js
+++ variablesAndDataTypes.js
@@ -21,10 +21,51 @@ console.log(decimalNumber);
 const adding = wholeNumber + decimalNumber; */
 
 // ** Booleans true or false
-const coolGuy = true;
+/* const coolGuy = true;
 
 if(coolGuy) {
     console.log('I am cool');
 } else {
     console.log('I am NOT cool');
 }
+
+//Null
+const age = null;
+
+console.log(age);
+
+// Undefined
+
+let x;
+
+console.log(x);  */
+
+// Objects
+
+const name = 'john';
+const age = 44;
+
+const person ={
+    name: 'john',
+    age: 44,
+}
+// Dot Notation
+//console.log(person.name);
+
+// Array
+const arr =[1, 2, 3, 4];
+//console.log(typeof arr);
+
+const date = new Date();
+//console.log(typeof date);
+
+// Static typed
+let message = 'Hello';
+//console.log(typeof 3);
+
+message = 5;
+
+//console.log(typeof message);
+
+let y = 3 / "bob ryan";
+console.log(typeof {});
