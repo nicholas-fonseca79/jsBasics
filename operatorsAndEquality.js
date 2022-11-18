c45dc84aa8dba161e54b132b308d7683f188d180
nick fonseca <nick.fonseca79@gmail.com>
2022-11-18 12:41:54 -0700

    operators and equality

---
+++ operatorsAndEquality.js
@@ -0,0 +1,35 @@
+/* // **Arithmetic Operators**
+//const a =10;
+//const b =12;
+let result =0;
+
+//addition
+// result = a + b;
+//subtraction
+result = a - b;
+//multiplication
+result = a * b;
+//division
+result = a / b;
+//exponent
+result = a ** b;
+result = b ** a;
+//modulo
+result = a % b;
+//increment
+result++; 
+//decrement
+//result--;
+console.log(result); */
+
+// **Comparison operators and Equality**
+
+const a = 5;
+const b = 10;
+
+//Greater Than
+console.log(a > b);
+// Greater than or equal
+console.log(a <= b);
+// Less than
+console.log(a > b);
